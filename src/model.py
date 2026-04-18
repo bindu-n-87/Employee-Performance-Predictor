282aa6893fc5cbb7c3a4c7e01d84c25032965f4a
bindu-n-87 <bindu.np18@gmail.com>
2026-04-18 16:45:33 +0530

    trained a Random Forest classifier to predict employee performance

--- src/model.py
+++ src/model.py
@@ -7,32 +7,20 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-# -----------------------------
-# LOAD DATA
-# -----------------------------
 df = pd.read_csv("data/employee_data.csv")
 
 # DROP ID
 df = df.drop("Employee_ID", axis=1)
 
-# -----------------------------
-# ENCODING
-# -----------------------------
 dept_encoder = LabelEncoder()
 df["Department"] = dept_encoder.fit_transform(df["Department"])
 
 target_encoder = LabelEncoder()
 df["Performance"] = target_encoder.fit_transform(df["Performance"])
 
-# -----------------------------
-# BALANCE CHECK (DEBUG INFO)
-# -----------------------------
 print("\nClass Distribution:")
 print(df["Performance"].value_counts())
 
-# -----------------------------
-# SPLIT DATA
-# -----------------------------
 X = df.drop("Performance", axis=1)
 y = df["Performance"]
 
@@ -43,9 +31,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y   # IMPORTANT FIX
 )
 
-# -----------------------------
-# MODEL
-# -----------------------------
+
 model = RandomForestClassifier(
     n_estimators=200,
     max_depth=12,
@@ -54,17 +40,11 @@ model = RandomForestClassifier(
 
 model.fit(X_train, y_train)
 
-# -----------------------------
-# EVALUATION
-# -----------------------------
 y_pred = model.predict(X_test)
 
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
 
-# -----------------------------
-# SAVE MODEL
-# -----------------------------
 os.makedirs("models", exist_ok=True)
 
 joblib.dump(model, "models/performance_model.pkl")
